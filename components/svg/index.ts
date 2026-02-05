6bd0c94858b004bb19b3eba3898740c585dac0a8
Lado <lado.asambadze1@gmail.com>
2026-02-05 13:54:38 +0400

    add translations

---
+++ components/svg/index.ts
@@ -0,0 +1,3 @@
+export { default as English } from "/svg/English.svg";
+export { default as Georgian } from "/svg/Georgian.svg";
+export { default as Russian } from "/svg/Russian.svg";
---
+++ i18n/request.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { getRequestConfig } from "next-intl/server";
+import { routing } from "./routing";
+
+export default getRequestConfig(async ({ requestLocale }) => {
+  let locale = await requestLocale;
+
+  if (!locale || !routing.locales.includes(locale as any)) {
+    locale = routing.defaultLocale;
+  }
+
+  const messages = {
+    main: (await import(`../messages/${locale}/main.json`)).default,
+  };
+
+  return {
+    locale,
+    messages,
+  };
+});
